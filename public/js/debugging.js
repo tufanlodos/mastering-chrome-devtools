b4f78427d411b261de3ebdc806de889e5e00e333
mertcelil <mert@lenasoftware.com>
2020-11-13 17:42:05 +0300

    fix: debugging exercise

--- public/js/debugging.js
+++ public/js/debugging.js
@@ -3,8 +3,10 @@ const fetcher = document.getElementById("fetcher");
 fetcher.addEventListener("click", fetchImages);
 
 function fetchImages() {
-  fetch("/api").then(data => {
-    processImages(data.imageList);
+  fetch("/api")
+  .then(response=>response.json())
+  .then(data => {
+    processImages(data.images);
   });
 }
 
@@ -23,8 +25,8 @@ function processImages(images) {
     author.classList.add("debug-author");
 
     // Populate elements
-    title.innerText = element.photographer;
-    author.innerText = ` by ${element.title}`;
+    title.innerText = element.name;
+    author.innerText = ` by ${element.photographer}`;
     image.src = element.source;
 
     // Append elements
@@ -33,4 +35,6 @@ function processImages(images) {
     item.appendChild(image);
     list.appendChild(item);
   });
+
+  document.body.appendChild(list);
 }
