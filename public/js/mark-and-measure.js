bccd076beecd4dcb365bdf8642597fb2cddd3cc8
mertcelilozdin@hotmail.com <mert@lenasoftware.com>
2020-11-20 12:29:49 +0300

    mark and measure solution

--- public/js/mark-and-measure.js
+++ public/js/mark-and-measure.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", thingToMeasure);
 
 function thingToMeasure() {
+  performance.mark("start");
   fetch("/api")
     .then(result => result.json())
     .then(json => {
       json.images.forEach(image => console.log(image.name));
+      performance.mark("end");
+      performance.measure("Fetching images","start","end");
+      const measurements = performance.getEntriesByType("measure");
+      console.log(measurements);
     });
 }
